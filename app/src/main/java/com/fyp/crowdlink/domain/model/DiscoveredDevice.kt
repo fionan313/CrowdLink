ffd6edf7aeffde51e96a400e334f6efae11a6e4f
fionan313 <fionanoceallaigh@gmail.com>
2025-11-29 16:51:41 +0000

    Resolved issue with package names

--- app/src/main/java/com/fyp/crowdlink/domain/model/DiscoveredDevice.kt
+++
@@ -1,8 +0,0 @@
-package com.fyp.crowdlink.domain.model
-
-data class DiscoveredDevice(
-    val deviceId: String,
-    val rssi: Int,
-    val timestamp: Double = System.currentTimeMillis().toDouble(),
-    val estimatedDistance: Double = -1.0
-)
