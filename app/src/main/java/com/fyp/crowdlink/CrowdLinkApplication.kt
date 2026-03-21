83c3d894e17f12e187e06b87ee5e46a249cc5a04
fionan313 <fionanoceallaigh@gmail.com>
2026-03-21 15:44:55 +0000

    chore: Initialise MapLibre in CrowdLinkApplication
    - Initialise MapLibre instance with `WellKnownTileServer.MapLibre` in `onCreate`
    - Clean up Timber plant comment and formatting in `CrowdLinkApplication`
    - Prevents app crashing when rendering map view.

--- app/src/main/java/com/fyp/crowdlink/CrowdLinkApplication.kt
+++ app/src/main/java/com/fyp/crowdlink/CrowdLinkApplication.kt
@@ -2,6 +2,8 @@ package com.fyp.crowdlink
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.maplibre.android.MapLibre
+import org.maplibre.android.WellKnownTileServer
 import timber.log.Timber
 
 @HiltAndroidApp
@@ -9,6 +11,8 @@ class CrowdLinkApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())  // ADD
+        Timber.plant(Timber.DebugTree())
+        // Initialise MapLibre once at app start
+        MapLibre.getInstance(this, "", WellKnownTileServer.MapLibre)
     }
 }
